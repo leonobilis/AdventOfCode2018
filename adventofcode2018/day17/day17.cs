c99076b159308b6661f34402e8bb57a26265ffd8
Marcin L <lev7@vp.pl>
2018-12-18 01:33:02 +0100

    Day 17 works!

--- adventofcode2018/day17/day17.cs
+++ adventofcode2018/day17/day17.cs
@@ -28,7 +28,7 @@ namespace adventofcode2018
             }
         }
 
-        public static int CountWaterTiles(IEnumerable<string> input)
+        public static (int, int) CountWaterTiles(IEnumerable<string> input)
         {
             Regex inputRx = new Regex(@"([xy])=([\d]+), [xy]=([\d]+)..([\d]+)", RegexOptions.Compiled);
             
@@ -38,105 +38,57 @@ namespace adventofcode2018
                             .ToDictionary(k => k.Key, v => v.Select (s => s.Item1).OrderBy(o => o).ToList());
             
             var maxY = clay.Keys.Max() + 1;
-            var minX = clay.Select(s => s.Value.Min()).Min() - 1;
-            var maxX = clay.Select(s => s.Value.Max()).Max() + 1;
+            var minY = clay.Keys.Min() - 1;
+            var minX = clay.Select(s => s.Value.Min()).Min() - 2;
+            var maxX = clay.Select(s => s.Value.Max()).Max() + 2;
 
             var water = new HashSet<(int, int)>();
-            var sources = new HashSet<(int, int)> { (500, 0) };
+            var spring = new HashSet<(int, int)>();
+            var sources = new HashSet<(int, int)> { (500, minY) };
             var sourcesComputed = new HashSet<(int, int)> { (500, 0) };
 
             while (sources.Count > 0)
             {
                 var newSources = new HashSet<(int, int)>();
-                //var result = sources.AsParallel()
-                //       .Select(s => ComputeSource(s, clay, water, maxY, minX, maxX)).ToList();
-                //.Aggregate((newSources, newWater), (acc, x) => (acc.Item1.Union(x.Item1), acc.Item2.Union(x.Item2)));
+                (newSources, water, spring) = sources.Select(s => ComputeSource(s, clay, water, maxY, minX, maxX))
+                                                     .Aggregate((newSources, water, spring), (acc, x) => (acc.Item1.Union(x.Item1).ToHashSet(),
+                                                                                                          acc.Item2.Union(x.Item2).ToHashSet(),
+                                                                                                          acc.Item3.Union(x.Item3).Except(acc.Item2).ToHashSet()));
 
-                foreach (var source in sources)
-                {
-                    var result = ComputeSource(source, clay, water, maxY, minX, maxX);
-                    newSources.UnionWith(result.Item1);
-                    sourcesComputed.UnionWith(sources);
-                    water.UnionWith(result.Item2);
-                    
-                }
+                sourcesComputed.UnionWith(sources);
                 sources = newSources.Except(sourcesComputed).ToHashSet();
-                if (water.Count > 42000)
-                  PrintResult(clay, minX, water);
 
             }
 
-
-            //for (int i = 0; i < maxY; ++i)
-            //{
-            //    Console.WriteLine();
-            //    for (int j = minX; j < maxX; ++j)
-            //    {
-            //        if (clay.Contains((j, i)))
-            //            Console.Write('#');
-            //        else if (water.Contains((j, i)))
-            //            Console.Write('O');
-            //        else
-            //            Console.Write(' ');
-
-            //    }
-            //}
-
-
-            return water.Count();
+            return (water.Count + spring.Count, water.Count);
         }
 
-        private static void PrintResult(Dictionary<int, List<int>> clay, int minX, HashSet<(int, int)> water)
+        private static (HashSet<(int, int)>, HashSet<(int, int)>, HashSet<(int, int)>) ComputeSource((int, int) source, Dictionary<int, List<int>> clay, HashSet<(int, int)> water, int maxY, int minX, int maxX)
         {
-            for (int i = water.Max(m => m.Item2) - 200; i < water.Max(m => m.Item2) + 15; ++i)
-            {
-                Console.WriteLine();
-                for (int j = minX; j < water.Max(m => m.Item1) + 5; ++j)
-                {
-                    if (clay.ContainsKey(i) && clay[i].Contains(j))
-                        Console.Write('#');
-                    else if (water.Contains((j, i)))
-                        Console.Write('O');
-                    else
-                        Console.Write(' ');
-
-                }
-            }
-            Console.WriteLine();
-            Console.ReadKey();
-        }
-
-        private static (List<(int, int)>, HashSet<(int, int)>) ComputeSource((int, int) source, Dictionary<int, List<int>> clay, HashSet<(int, int)> water, int maxY, int minX, int maxX)
-        {
-            var newSources = new List<(int, int)>();
-            var sourceEnd = clay.Where(x => x.Key > source.Item2 && x.Value.Contains(source.Item1)).Select(s => s.Key)/*.Union(water.Where(x => x.Item1 == source.Item1 && x.Item2 > source.Item2).Select(s => s.Item2)).*/.DefaultIfEmpty(maxY).Min();
-            { }
-            var newWater = Enumerable.Range(source.Item2 + 1, sourceEnd - source.Item2 - 1).Select(s => (source.Item1, s)).ToHashSet(); // ToDictionary(k => k, v => new List<int> { source.Item1 });
+            var newSources = new HashSet<(int, int)>();
+            var sourceEnd = clay.Where(x => x.Key > source.Item2 && x.Value.Contains(source.Item1)).Select(s => s.Key).DefaultIfEmpty(maxY).Min();
+            var spring = Enumerable.Range(source.Item2 + 1, sourceEnd - source.Item2 - 1).Select(s => (source.Item1, s)).ToHashSet();
+            
+            var newWater = new HashSet<(int, int)>();
 
             if (clay.ContainsKey(sourceEnd))
             {
                 bool noHole = true;
                 for (var current = (source.Item1, sourceEnd - 1); noHole; current.Item2--)
                 {
-                    //var clayAndWater = clay.GetValueOrDefault(current.Item2, new List<int>()).Union(water.Where(x => x.Item2 == current.Item2).Select(s => s.Item1));
-                    var left = clay.GetValueOrDefault(current.Item2, new List<int>()).Where(c => c < current.Item1).DefaultIfEmpty(minX).Max();
-                    var right = clay.GetValueOrDefault(current.Item2, new List<int>()).Where(c => c > current.Item1).DefaultIfEmpty(maxX).Min();
-                    left++;
-                    right--;
-
-                    //clayAndWater = clay.GetValueOrDefault(current.Item2 + 1, new List<int>()).Union(water.Where(x => x.Item2 == current.Item2 + 1).Select(s => s.Item1));
+                    var left = clay.GetValueOrDefault(current.Item2, new List<int>()).Where(c => c < current.Item1).DefaultIfEmpty(minX).Max() + 1;
+                    var right = clay.GetValueOrDefault(current.Item2, new List<int>()).Where(c => c > current.Item1).DefaultIfEmpty(maxX).Min() - 1;
 
                     if (clay.ContainsKey(current.Item2 + 1))
                     {
-
                         var leftHole = clay[current.Item2 + 1].Where((c, i) => c <= current.Item1 && c >= left && !clay[current.Item2 + 1].Contains(c - 1)).DefaultIfEmpty(minX).Max();
                         if (leftHole > left)
                         {
                             newSources.Add((leftHole - 1, current.Item2));
-                            newWater.Add((leftHole - 1, current.Item2));
-                            left = leftHole - 1;
+                            spring.Add((leftHole - 1, current.Item2));
                             if (current.Item2 != source.Item2)
                             {
+                                left = leftHole - 1;
                                 noHole = false;
                             }
                         }
@@ -144,7 +96,7 @@ namespace adventofcode2018
                         if (rightHole < right)
                         {
                             newSources.Add((rightHole + 1, current.Item2));
-                            newWater.Add((rightHole + 1, current.Item2));
+                            spring.Add((rightHole + 1, current.Item2));
                             if (current.Item2 != source.Item2)
                             {
                                 right = rightHole + 1;
@@ -153,27 +105,31 @@ namespace adventofcode2018
                         }
                     }
 
-                    newWater.UnionWith(Enumerable.Range(left, right - left + 1).Select(s => (s, current.Item2)));
+                    if (noHole)
+                        newWater.UnionWith(Enumerable.Range(left, right - left + 1).Select(s => (s, current.Item2)));
+                    else
+                        spring.UnionWith(Enumerable.Range(left, right - left + 1).Select(s => (s, current.Item2)));
                 }
             }
 
-            return (newSources, newWater);
+            return (newSources, newWater, spring);
         }
 
         public static void Solution()
         {
-            var testInput = @"x=495, y=2..7
-y=7, x=495..501
-x=501, y=3..7
-x=498, y=2..4
-x=506, y=1..2
-x=498, y=10..13
-x=504, y=10..13
-y=13, x=498..504".Split("\n");
-              Console.WriteLine(CountWaterTiles(testInput));
+//             var testInput = @"x=495, y=2..7
+// y=7, x=495..501
+// x=501, y=3..7
+// x=498, y=2..4
+// x=506, y=1..2
+// x=498, y=10..13
+// x=504, y=10..13
+// y=13, x=498..504".Split("\n");
+//               Console.WriteLine($"{CountWaterTiles(testInput).Item1}, {CountWaterTiles(testInput).Item2}");
 
             var input = GetFromFile(17);
-            Print(CountWaterTiles(input));
+            var result = CountWaterTiles(input);
+            Print(result.Item1, result.Item2);
         }
     }
 }
