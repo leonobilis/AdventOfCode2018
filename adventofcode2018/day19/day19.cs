c220fc15fff5322b760fb890dececd6c6bd8a0a0
Marcin L <lev7@vp.pl>
2018-12-19 08:24:52 +0100

    Unnecessary imports removed

--- adventofcode2018/day19/day19.cs
+++ adventofcode2018/day19/day19.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
-using System.IO;
 
 namespace adventofcode2018
 {
