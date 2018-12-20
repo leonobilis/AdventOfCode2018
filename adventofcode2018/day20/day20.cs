2bee5a7a22e92ac386bcc8485a4ec3712c3d52f2
lev7 <lev7@vp.pl>
2018-12-20 18:05:00 +0100

    Day 20

--- adventofcode2018/day20/day20.cs
+++ adventofcode2018/day20/day20.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using QuickGraph;
 using QuickGraph.Algorithms;
+using System.Threading;
 
 namespace adventofcode2018
 {
@@ -12,117 +13,109 @@ namespace adventofcode2018
     using Edge = Edge<(int, int)>;
     using Map = UndirectedGraph<(int, int), Edge<(int, int)>>;
 
-        // public void TryMove(IEnumerable<Unit> units)
-        // {
-
-        //     IEnumerable<Edge> path;
-        //     IEnumerable<Edge> shorterPath = null;
-        //     Func<Edge, double> edgeCost = e => 1;
-
-        //     foreach (var range1 in EmptyInRange)
-        //     {
-        //         var tryPath = map.ShortestPathsDijkstra(edgeCost, range1);
-        //         foreach (var range2 in EnemiesRanges)
-        //         {
-        //             if (tryPath(range2, out path))
-        //                 shorterPath = GetShorterPath(shorterPath, path);
-        //         }
-        //     }
-        // }
-
     public static class Day20
     {
-        static (int, Field) Traverse(IEnumerable<char> input, Map map, Field current)
+        static (IEnumerable<char>, HashSet<Field> current) Traverse(IEnumerable<char> input, Map map, HashSet<Field> current)
         {
-            for(var i = 0; i < input.Count(); ++i)
+            return current.Select(s => Traverse(input, map, s)).Aggregate((input, new HashSet<Field>()), (acc, x) => (x.Item1, acc.Item2.Union(x.current).ToHashSet()));
+        }
+
+        static (IEnumerable<char>, HashSet<Field> current) Traverse(IEnumerable<char> input, Map map, Field current)
+        {
+            HashSet<Field> newCurrent = null;
+            var a = input.First();
+
+            switch (input.First())
             {
-                switch(input.Skip(i).First())
-                {
-                    case 'N':
-                        map.AddVerticesAndEdge(new Edge(current, (current.Item1, current.Item2 - 1)));
-                        current = (current.Item1, current.Item2 + 1);
-                        break;
-                    case 'S':
-                        map.AddVerticesAndEdge(new Edge(current, (current.Item1, current.Item2 + 1)));
-                        current = (current.Item1, current.Item2 + 1);
-                        break;
-                    case 'E':
-                        map.AddVerticesAndEdge(new Edge(current, (current.Item1 + 1, current.Item2)));
-                        current = (current.Item1 + 1, current.Item2);
-                        break;
-                    case 'W':
-                        map.AddVerticesAndEdge(new Edge(current, (current.Item1 - 1, current.Item2)));
-                        current = (current.Item1 - 1, current.Item2);
-                        break;
-                    case '(':
-                        var fieldsToTraverse = new HashSet<Field>();
-                        for (int ii; input.Skip(i).First() != ')';  i += ii)
-                        {
-                            Field fieldToCheck;
-                            (ii, fieldToCheck) = Traverse(input.Skip(i+1), map, current);
-                            fieldsToTraverse.Add(fieldToCheck);
-                        }
-                        fieldsToTraverse.ToList().ForEach( c => Traverse(input.Skip(i+1), map, c) );
-                        return (i, current);
-                    case '|':
-                    case ')':
-                    case '$':
-                        return (i+1, current);
-                }
+                case 'N':
+                    map.AddVerticesAndEdge(new Edge(current, (current.Item1, current.Item2 - 1)));
+                    current = (current.Item1, current.Item2 - 1);
+                    return Traverse(input.Skip(1), map, current);
+                case 'S':
+                    map.AddVerticesAndEdge(new Edge(current, (current.Item1, current.Item2 + 1)));
+                    current = (current.Item1, current.Item2 + 1);
+                    return Traverse(input.Skip(1), map, current);
+                case 'E':
+                    map.AddVerticesAndEdge(new Edge(current, (current.Item1 + 1, current.Item2)));
+                    current = (current.Item1 + 1, current.Item2);
+                    return Traverse(input.Skip(1), map, current);
+                case 'W':
+                    map.AddVerticesAndEdge(new Edge(current, (current.Item1 - 1, current.Item2)));
+                    current = (current.Item1 - 1, current.Item2);
+                    return Traverse(input.Skip(1), map, current);
+                case '(':
+                    newCurrent = new HashSet<Field>();
+                    while (input.First() != ')')
+                    {
+                        HashSet<Field> fieldsToCheck;
+                        (input, fieldsToCheck) = Traverse(input.Skip(1), map, current);
+                        newCurrent.UnionWith(fieldsToCheck);
+                    }
+                    return Traverse(input.Skip(1), map, newCurrent);
+                case '|':
+                case ')':
+                    return (input, new HashSet<Field> { current });
+                case '$':
+                    return (input, new HashSet<Field> {});
+                    
             }
 
-            return (0, current);
+            return Traverse(input.Skip(1), map, newCurrent);
         }
 
         static Map GetMap(IEnumerable<char> input)
         {
             var map = new Map();
-            Traverse(input.Skip(1), map, (0, 0));
-            
-            map.AddVertexRange(filteredGrid.Select(s => s.Key));
-            var edges = filteredGrid.Where(x => grid[x.Key.Item1-1][x.Key.Item2] == '.')
-                                    .Select(s => new Edge(s.Key, (s.Key.Item1-1, s.Key.Item2)));
-            map.AddEdgeRange(edges);
-            edges = filteredGrid.Where(x => grid[x.Key.Item1][x.Key.Item2-1] == '.')
-                                .Select(s => new Edge(s.Key, (s.Key.Item1, s.Key.Item2-1)));
-            map.AddEdgeRange(edges);
-            edges = filteredGrid.Where(x => grid[x.Key.Item1][x.Key.Item2+1] == '.')
-                                    .Select(s => new Edge(s.Key, (s.Key.Item1, s.Key.Item2+1)));
-            map.AddEdgeRange(edges);
-            edges = filteredGrid.Where(x => grid[x.Key.Item1+1][x.Key.Item2] == '.')
-                                .Select(s => new Edge(s.Key, (s.Key.Item1+1, s.Key.Item2)));
-            map.AddEdgeRange(edges);
-
+            int stackSize = 1024 * 1024 * 64;
+            Thread th = new Thread(() => Traverse(input.Skip(1), map, (0, 0)), stackSize);
+            th.Start();
+            th.Join();
             return map;
         }
 
-        public static int DoorsToFurthest(IEnumerable<char> input)
+        static (int max, int paths1k) DoorsToFurthest(IEnumerable<char> input)
         {
             var map = GetMap(input);
-            var round = 0;
 
-            return 0;
+            IEnumerable<Edge> path;
+            Func<Edge, double> edgeCost = e => 1;
+
+            var tryPath = map.ShortestPathsDijkstra(edgeCost, (0, 0));
+            var max = 1000;
+            var paths1k = 0;
+
+            foreach (var room in map.Vertices)
+            {
+                if (tryPath(room, out path) && path.Count() >= 1000)
+                {
+                    ++paths1k;
+                    max = path.Count() > max ? path.Count() : max;
+                }
+            }
+
+            return (max, paths1k);
         }
 
         public static void Solution()
         {
-            var testInput = "^WNE$";
-            Console.WriteLine(DoorsToFurthest(testInput));
+            //var testInput = "^WNE$";
+            //Console.WriteLine(DoorsToFurthest(testInput));
 
-            var testInput2 = "^ENWWW(NEEE|SSE(EE|N))$";
-            Console.WriteLine(DoorsToFurthest(testInput2));
+            //var testInput2 = "^ENWWW(NEEE|SSE(EE|N))$";
+            //Console.WriteLine(DoorsToFurthest(testInput2));
 
-            var testInput3 = "^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$";
-            Console.WriteLine(DoorsToFurthest(testInput3));
+            //var testInput3 = "^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$";
+            //Console.WriteLine(DoorsToFurthest(testInput3));
 
-            var testInput4 = "^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$";
-            Console.WriteLine(DoorsToFurthest(testInput4));
+            //var testInput4 = "^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$";
+            //Console.WriteLine(DoorsToFurthest(testInput4));
 
-            var testInput5 = "^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$";
-            Console.WriteLine(DoorsToFurthest(testInput5));
+            //var testInput5 = "^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$";
+            //Console.WriteLine(DoorsToFurthest(testInput5));
             
             var input = GetFromFile(20).First();
-            Print(DoorsToFurthest(input));
+            var result = DoorsToFurthest(input);
+            Print(result.max, result.paths1k);
         }
     }
 }
